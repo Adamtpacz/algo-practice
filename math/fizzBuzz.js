afae13af6a138563fb1e96e685572a6ac487b345
Adamtpacz <adamtpacz@gmail.com>
2023-06-26 11:24:20 -0400

    Complete 'Fizz Buzz'

---
+++ math/fizzBuzz.js
@@ -0,0 +1,22 @@
+let fizzBuzz = function(n) {
+    answer = []
+    for (let i = 1; i <= n; i++) {
+        if (i % 5 == 0 && i % 3 == 0) {
+            answer.push('FizzBuzz')
+        }
+        else if (i % 3 == 0) {
+            answer.push('Fizz')
+        }
+        else if (i % 5 == 0) {
+            answer.push('Buzz')
+        }
+        else {
+            answer.push(i.toString())
+        }
+    }
+    return answer
+}
+
+n = 15
+
+console.log(fizzBuzz(n))
